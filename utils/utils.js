3cbed0fcee29335ffec887afabcbcfc7af4cf595
Jeremy Lincoln <bossco28@gmail.com>
2017-11-22 10:08:27 -0500

    Add in a new test for squaring

--- utils/utils.js
+++ utils/utils.js
@@ -1,2 +1,4 @@
 //since we are just returning. We don't need the return keyword or brackets
 module.exports.add = (a, b) => a + b;
+
+module.exports.square = (x) => x * x + 1;
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -2,5 +2,17 @@ const utils = require('./utils');
 
 //this is BDD
 it('should add two numbers', () => {
-    var result = utils.add(33, 11);
+    var res = utils.add(33, 11);
+    
+    if(res !== 44){
+        throw new Error(`Expected 44, but got ${res}.`);
+    }
 });
+
+it('should square a number', () => {
+    var res = utils.square(2);
+
+    if(res !== 4) {
+        throw new Error(`Expected 4, but got ${res}.`)
+    }
+})
